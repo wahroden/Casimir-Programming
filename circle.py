d5dead32b76295e3de539a503252adfd66efc7ae
wahroden <w.a.h.roden@tudelft.nl>
2017-11-09 15:56:28 +0100

    a little contribution

--- circle.py
+++ circle.py
@@ -4,6 +4,7 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 #import scipy as sc
 #import numpy as np
+print("Enter the coordinates of the circle and the radius within (0 to 10) individually")
 x=float(input())
 y=float(input())
 radius=float(input())
@@ -15,5 +16,7 @@ fig, ax= plt.subplots()
 ax.add_artist(circle)
 ax.set_xlim((-10,10))
 ax.set_ylim((-10,10))
+plt.xlabel('X')
+plt.ylabel('Y')
 plt.show()
 fig.savefig('circle1.png')
